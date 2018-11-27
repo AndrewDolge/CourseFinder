0d64b8a1e4019bbd71f69324d31338b58f7b79fc
linh0006 <43328091+linh0006@users.noreply.github.com>
2018-11-27 09:35:44 -0600

    More edits to homePage

--- homePage.js
+++ homePage.js
@@ -1,4 +1,5 @@
 
+
 var fs = require('fs');
 var http = require('http');
 var url = require('url'); 
@@ -45,8 +46,47 @@ app.get('/home', (req, res) => {
    
 });
 
-//When user tries to login/create a new user 
-app.post('/login', (req, res) => {
+//When retuning user logs in
+app.post('/login' , (req, res) => {
+	var login = 0;
+	var pass = '';
+	var hash = '';
+	var form = new multiparty.Form();
+    form.parse(req, (err, fields, files) => {
+		
+		login = fields.login;
+		pass = fields.password;
+	
+		//if(login != '' && pass != ''){
+		if(login == '' || pass == ''){
+			errorLog(res,"Please enter a login and password");
+		}
+		else{
+			ust_db.all("Select * From People where university_id == ?",login, (err, rows) => {
+				if (err) {
+					console.log('Error running query');
+				}
+				else {
+					//If user tries to login but no username exists
+					if(rows.length === 0){
+						errorLog(res,"Username does not exist! Please create new user");
+			
+					}
+					//If user tries to login and user name exists
+					else{
+						
+					}//else
+				}//else
+			}); //ust_db.all
+		} //else
+		
+
+	}); //form.parse
+
+}); //app.post(/login)
+
+//When user tries to create new account 
+app.post('/new', (req, res) => {
 	
 	var login = 0;
 	var pass = '';
@@ -57,52 +97,39 @@ app.post('/login', (req, res) => {
 		login = fields.login;
 		pass = fields.password;
 	
-		if(login != '' && pass != ''){
+		
+		if(login == '' || pass == ''){
+			errorLog(res,"Please enter a login and password");
+		}
+		else{
 			ust_db.all("Select * From People where university_id == ?",login, (err, rows) => {
 				if (err) {
 					console.log('Error running query');
 				}
-				else {
-					
+				else {	
 					if(rows.length === 0){
-						
 						//having trouble inserting multiple things at once, need to remember the escape 
 						hash = md5(pass);
 						//ust_db.run("INSERT INTO People(password) VALUES(?)", hash);
 						ust_db.run("INSERT INTO People(university_id) VALUES(?)",login);
 						
 						//after insert here do a read file to our search page
-			
 					}
 					else{
+						errorLog(res,"Cannot create user with existing login!");
 						//if user tries to create a new user that already exists returns to home page with adding a warning banner 
-						fs.readFile(path.join(public_dir, 'index.html'), (err, data) => { 
-							if(err){
-								res.writeHead(404, {'Content-Type': 'text/plain'});
-								res.write('Oh no! Could\'t find that page!'); 
-								res.end();
-							}  
-							
-							else{
-								var mime_type= mime.lookup('index.html') || 'text/html'; //if cant find mime type of file then content type is text/plain, mime.lookup will give valid mime type of file or  return false
-								res.writeHead(200, {'Content-Type': mime_type}); //for each file make the content type the mime type in order to properly display 
-								//Warning banner 
-								res.write('<h3 style= "color: white; background-color: orange; text-align: center;"> Cannot create user with existing login!</h3>');
-								res.write(data); //write the content received in data
-								res.end();
-							} 
-						}); //fs.readFile 
+						
 					}//else
 				}//else
 			}); //ust_db.all
-		} //if
+		} //else
 		
 
 		
-	});
+	}); //form.parse
 	
 
-});
+}); //app.post(/new);
 
 
 
@@ -112,8 +139,26 @@ app.listen(port, () => {
  
 
 
+function errorLog(res,reason){
+	fs.readFile(path.join(public_dir, 'index.html'), (err, data) => { 
+		if(err){
+			res.writeHead(404, {'Content-Type': 'text/plain'});
+			res.write('Oh no! Could\'t find that page!'); 
+			res.end();
+		}  
+		
+		else{
+			var mime_type= mime.lookup('index.html') || 'text/html'; 
+			res.writeHead(200, {'Content-Type': mime_type}); 
+			//Warning banner 
+			res.write('<h3 style= "color: white; background-color: orange; text-align: center;">'+reason+'</h3>');
+			res.write(data); //write the content received in data
+			res.end();
+		} 
+	}); //fs.readFile 
+	
 
-
+}
 
 
 
